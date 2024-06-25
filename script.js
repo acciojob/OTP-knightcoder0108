8238db5735b0933d2acdea9899cfd1cec998814e
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-06-25 19:34:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const codes = document.querySelectorAll(".code");
+    
+    codes.forEach((code, index) => {
+        code.addEventListener("keydown", (e) => {
+            if (e.key >= 0 && e.key <= 9) {
+                codes[index].value = "";
+                setTimeout(() => {
+                    if (index < codes.length - 1) {
+                        codes[index + 1].focus();
+                    }
+                }, 10);
+            } else if (e.key === "Backspace") {
+                setTimeout(() => {
+                    if (index > 0) {
+                        codes[index - 1].focus();
+                    }
+                }, 10);
+            }
+        });
+    });
+});
